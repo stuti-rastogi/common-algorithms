7efa48d3e5c5d84705794ef6f4cb9f1a98721ea0
Stuti Rastogi <stutirastogi.usc+github@gmail.com>
2021-03-23 17:02:57 +0530

    Adding alternate selectionSort

--- selectionSort.py
+++ selectionSort.py
@@ -19,9 +19,25 @@ def selectionSort(arr):
 
     return
 
+##---------------------------------------------------------------------------##
+
+# instead of helper function we use double for loop
+def selectionSortSimple(arr):
+    n = len(arr)
+
+    for i in range(n):
+        minIndex = i
+        for j in range(i+1, n):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        if i != minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+    return
+
 
 # test
 arr = [5, 3, 0, 1, 4, 2]
 print ("Original array:\t{}".format(arr))
-selectionSort(arr)
+selectionSortSimple(arr)
 print ("Sorted array:\t{}".format(arr))
